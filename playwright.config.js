9b5400edc7c2e75b1649fcedc14e9ef3bd69f63b
Pattyd08 <patriciada_br@yahoo.com.br>
2026-06-21 12:49:00 -0300

    feat: configura allure para abrir direto no github pages

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [
+    ['html'], // Seu relatório padrão que já funciona
+    ['allure-playwright', { outputFolder: 'allure-results' }] // O Allure gerando os dados brutos
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
